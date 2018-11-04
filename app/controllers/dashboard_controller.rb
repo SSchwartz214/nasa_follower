8c9654229b435c2bff9b74791adad0083946736d
Seth Schwartz <sschwartz214@gmail.com>
2018-11-03 18:31:12 -0600

    Creates faraday method to Return picture of the day from NASA API

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    @conn = Faraday.new(url: "https://api.nasa.gov") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  
+    response = @conn.get("/planetary/apod?api_key=ZjWqIom0yjCrJgCon701RFNIEjrfwH9d6uKRoVEk")
+  
+    @pod = JSON.parse(response.body, symbolize_names: true)[:hdurl]
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @pod = DashboardController.new.index
   end
 end
