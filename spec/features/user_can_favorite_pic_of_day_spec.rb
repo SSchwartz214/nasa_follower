03204968231e47373ae0aaf211b84e07378b46c6
Seth Schwartz <sschwartz214@gmail.com>
2018-11-06 17:18:54 -0700

    Adds tests for favoriting and unfavoriting

--- spec/features/user_can_favorite_pic_of_day_spec.rb
+++ spec/features/user_can_favorite_pic_of_day_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 describe 'As a registered user' do
   it 'can favorite a picture of the day' do
     PicOfDay.create(image_url: "https://via.placeholder.com/350x150")
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/'
+
+    click_on "Add to Favorites"
+
+    expect(current_path).to eq('/favorites')
+  end
+
+  it 'can remove a favorite' do
+    PicOfDay.create(image_url: "https://via.placeholder.com/350x150")
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/'
 
@@ -10,8 +24,10 @@ describe 'As a registered user' do
 
     expect(current_path).to eq('/favorites')
 
-    within('.container') do
-      expect(page).to have_css('.picture')
-    end
+    visit '/'
+
+    click_on "Remove from Favorites"
+
+    expect(current_path).to eq('/')
   end
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:pic_of_day_id) }
+  end
 end
